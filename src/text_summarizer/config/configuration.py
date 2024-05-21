42300127e734e43af02b15b2dc047d783dc8a768
Arjun Ravi <arjunravi1523@gmail.com>
2024-05-21 19:55:40 +0530

    Data ingestion modified

---
+++ src/text_summarizer/config/configuration.py
@@ -0,0 +1,28 @@
+from src.text_summarizer.constant import *
+from src.text_summarizer.utils.common import read_yaml, create_directories
+from src.text_summarizer.entity import DataIngestionConfig
+
+
+class ConfigurationManager:
+    def __init__(
+        self, config_file_path=CONFIG_FILE_PATH, params_file_path=PARAMS_FILE_PATH
+    ):
+
+        self.config = read_yaml(config_file_path)
+        self.params = read_yaml(params_file_path)
+
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir,
+        )
+
+        return data_ingestion_config
