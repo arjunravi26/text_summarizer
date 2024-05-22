0a6e0f4c4c3485fb0e02b95019491d08461c4d29
Arjun Ravi <arjunravi1523@gmail.com>
2024-05-22 11:57:58 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from text_summarizer.logging import logger
-from src.text_summarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from src.text_summarizer.pipeline.stage01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
 from src.text_summarizer.pipeline.stage_02_data_validation import DataValidationPipeline
-from src.text_summarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
-
-
-
+from src.text_summarizer.pipeline.stage_03_data_transformation import (
+    DataTransformationPipeline,
+)
 
 
 STAGE_NAME = "DATA INGESTION STAGE"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         logger.info(f"Starting>>>> {STAGE_NAME}")
         data_ingestion_pipeline = DataIngestionTrainingPipeline()
@@ -22,7 +23,7 @@ if __name__ == '__main__':
         data_ingestion_pipeline = DataTransformationPipeline()
         data_ingestion_pipeline.start()
         logger.info(f"Completed {STAGE_NAME}")
-        
+
     except Exception as e:
         logger.exception(e)
         raise e
